d77e4746117d7351276fef4474bb7ad09494bcd1
Jeremy IBANEZ <jibanez@e1r5p15.42.fr>
2014-06-10 19:15:30 +0200

    solution temporaire dans draw.c+ rajouter un delai

--- sources/gfx/draw.c
+++ sources/gfx/draw.c
@@ -6,7 +6,7 @@
 /*   By: jvincent <jvincent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/04 17:53:39 by jvincent          #+#    #+#             */
-/*   Updated: 2014/06/10 16:32:25 by jvincent         ###   ########.fr       */
+/*   Updated: 2014/06/10 19:13:49 by jibanez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,26 +26,25 @@ void	draw_menu(t_env *gfx)
 
 void	draw_tiles(t_env *gfx)
 {
-	SDL_Rect	rcsrc;
-	SDL_Rect	rcdest;
+	SDL_Rect	rc[2];
 	int			i;
 	int			j;
 
-	rcsrc.x = 0;
-	rcsrc.y = 200;
-	rcsrc.w = TILE_W;
-	rcsrc.h = TILE_H;
-	rcdest.w = TILE_W;
-	rcdest.h = TILE_H;
-	j = 0;
+	rc[0].x = 0;
+	rc[0].y = 200;
+	rc[0].w = TILE_W;
+	rc[0].h = TILE_H;
+	rc[1].w = TILE_W;
+	rc[1].h = TILE_H;
+	j = gfx->camera[0];
 	while (j < MAP_Y)
 	{
-		i = 0;
-		rcdest.y = j * TILE_H;
+		i = gfx->camera[1];
+		rc[1].y = j * TILE_H;
 		while (i < MAP_X)
 		{
-			rcdest.x = i * TILE_W;
-			SDL_RenderCopy(gfx->e.render, gfx->e.tiles, &rcsrc, &rcdest);
+			rc[1].x = i * TILE_W;
+			SDL_RenderCopy(gfx->e.render, gfx->e.tiles, &rc[0], &rc[1]);
 			i++;
 		}
 		j++;
@@ -55,14 +54,19 @@ void	draw_tiles(t_env *gfx)
 void	draw_board(t_env *gfx)
 {
 	SDL_Rect	rcdest;
+	static int	first = 0;
 
-//	SDL_SetRenderTarget(gfx->e.render, gfx->e.board);
-//	SDL_RenderClear(gfx->e.render);
-	draw_tiles(gfx);
+	if (!first)
+	{
+		SDL_SetRenderTarget(gfx->e.render, gfx->e.board);
+		SDL_RenderClear(gfx->e.render);
+		draw_tiles(gfx);
+		first = 1;
+	}
 	rcdest.x = gfx->camera[0];
 	rcdest.y = gfx->camera[1];
-//	rcdest.w = MAP_W;
-//	rcdest.h = MAP_H;
-//	SDL_SetRenderTarget(gfx->e.render, NULL);
-//	SDL_RenderCopy(gfx->e.render, gfx->e.board, NULL, &rcdest);
+	rcdest.w = MAP_W;
+	rcdest.h = MAP_H;
+	SDL_SetRenderTarget(gfx->e.render, NULL);
+	SDL_RenderCopy(gfx->e.render, gfx->e.board, NULL, &rcdest);
 }
