a1841664962b451c02d902b3be6e8df797570e81
npineau <nofigh@hotmail.fr>
2014-06-16 17:37:48 +0200

    tna now initialize all players' id in the team ay -2

--- sources/gfx/tna.c
+++ sources/gfx/tna.c
@@ -6,13 +6,14 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/16 15:29:25 by npineau           #+#    #+#             */
-/*   Updated: 2014/06/16 15:34:22 by npineau          ###   ########.fr       */
+/*   Updated: 2014/06/16 17:37:11 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	tna(t_env *gfx, char **param)
 {
 	int		i;
+	int		j;
 
 	i = 0;
 	while (i < 256)
@@ -20,6 +21,9 @@ void	tna(t_env *gfx, char **param)
 		if (gfx->g->teams[i]->name == NULL)
 		{
 			gfx->g->teams[i]->name = ft_strdup(param[1]);
+			j = 0;
+			while (j < 256)
+				gfx->g->teams[i]->players[j++] = -2;
 			break ;
 		}
 		i++;
