ef104be3aebf75f1528d1aa7ad5a227f24a6a903
npineau <nofigh@hotmail.fr>
2014-06-17 12:46:08 +0200

    moved pdi

---
+++ sources/gfx/pdi.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pdi.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/06/17 12:41:00 by npineau           #+#    #+#             */
+/*   Updated: 2014/06/17 12:45:15 by npineau          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	"gfx.h"
+
+void	pdi(t_env *gfx, char **param)
+{
+	t_player	*current;
+
+	current = get_player(gfx, ft_atoi(param[1]), NULL, PLAYER);
+	current->type = VOID;
+	current->id = -1;
+}
