d93d5ff15c0dc66769f298b9d7380209d6ddeee8
npineau <nofigh@hotmail.fr>
2014-06-25 15:45:24 +0200

    removed trash from ft_arrlen

--- sources/gfx/ft_arrlen.c
+++ sources/gfx/ft_arrlen.c
@@ -6,7 +6,7 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/25 14:07:06 by npineau           #+#    #+#             */
-/*   Updated: 2014/06/25 14:50:49 by npineau          ###   ########.fr       */
+/*   Updated: 2014/06/25 15:25:08 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,8 @@ int	ft_arrlen(void **array)
 {
 	int	len;
 
-	ft_putendl("ft_arrlen in");
 	len = 0;
 	while (*array++)
 		len ++;
-	ft_putendl("ft_arrlen in");
 	return (len);
 }
