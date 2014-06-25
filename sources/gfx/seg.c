92778e56d94058781eb21f4b1f4e82088ffe9fe8
npineau <nofigh@hotmail.fr>
2014-06-25 16:49:01 +0200

    added a bit of trash in seg

--- sources/gfx/seg.c
+++ sources/gfx/seg.c
@@ -6,7 +6,7 @@
 /*   By: jibanez <jibanez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/17 16:46:54 by jibanez           #+#    #+#             */
-/*   Updated: 2014/06/25 14:38:52 by npineau          ###   ########.fr       */
+/*   Updated: 2014/06/25 16:48:09 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,8 @@ void	seg(t_env *gfx, char **param)
 {
 	if (ft_arrlen((void **)param) != 2)
 		return ;
-	gfx->shm.shm->winner = ft_strdup(param[1]);
+	gfx->shm.shm->winner = ft_strtrimc(param[1], '\n');
+	ft_putstr("team ");
+	ft_putstr(gfx->shm.shm->winner);
+	ft_putendl(" win !");
 }
